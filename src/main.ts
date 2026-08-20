f24673608a1d7219d1916adf38b5dc4d5bf2a34f
xbl <18551736795@163.com>
2026-08-20 16:02:46 +0800

    feat: Integrate Element Plus icons into Vue application
    - Imported Element Plus icons and registered them as global components in the Vue app.
    - Enhanced the user interface by making icon components readily available throughout the application.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
